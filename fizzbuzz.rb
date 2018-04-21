b10695008d7a18c99efdbf6c5f33269661521ac9
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-21 02:57:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz (num)
   if num % 3 == 0
-    puts "Fizz" 
+    puts "Fizz"
   elsif num % 5 == 0
     puts "Buzz"
   elsif num % 5 && num % 3 == 0
     puts "FizzBuzz"
-  else 
+  else
     puts nil
 
 
